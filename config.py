533bb9b1ccc5919716fce6fde342cb9d45f861b3
2326wz <52245655+2326wz@users.noreply.github.com>
2020-12-07 20:25:08 +0300

    07-12

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ n_foil_params = 8
 n_foil_points = 128
 foils_dat_path = "./Foils DB/dat"
 foils_pkl_path = "./Foils DB/pkl"
+weights_path = "./weights"
 alfa_min = -2.1
 alfa_max = 8.
 re_min = 40000
@@ -11,3 +12,6 @@ re_max = 200000
 max_nans_in_curve = 0.6
 flap_position = 0.7
 xfoil_max_iterations = 64
+
+train_percentage = 0.75
+val_percentage = 0.9
